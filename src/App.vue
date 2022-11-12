27c1315da2d3647db0699549682e8abe32d98c99
edgarsanchez01 <a3521110095@alumno.uttehuacan.edu.mx>
2022-11-11 20:59:15 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="nav">
+    <nav-bar />
+
+    <navbar />
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+
+    <router-view />
+  </div>
+
+</template>
+
+<script>
+import Navbar from './components/NavBar.vue';
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
